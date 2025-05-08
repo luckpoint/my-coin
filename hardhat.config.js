83b24702b897d630c268ab28ec7f3accbef8dced
luckpoint <luckpoint@users.noreply.github.com>
2025-05-09 08:34:23 +0900

    work in progress...

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.28",
   networks: {
+    hardhat: {
+      // No specific URL or accounts needed for the default Hardhat network
+    },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
